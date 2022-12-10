0c3d93891f9dc5ab599650ebd0537dfd07298c8e
Warhorst <->
2022-12-10 16:12:48 +0100

    Implemented neighbours, position iter, addition and from tuple.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,249 @@
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+use std::ops::Add;
+use Direction::*;
+
+/// A 2D-point with signed x and y values. Used for boards and similar stuff.
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Position {
     x: isize,
-    y: isize
+    y: isize,
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+impl Position {
+    pub fn new(x: isize, y: isize) -> Self {
+        Position { x, y }
+    }
+
+    /// Return only neighbours in cardinal directions which are in the given bounds.
+    pub fn neighbours_in_bounds(&self, start: Position, end: Position) -> impl IntoIterator<Item=Position> {
+        self.neighbours()
+            .into_iter()
+            .filter(move |pos| *pos >= start && *pos <= end)
+    }
+
+    /// Return the neighbours in all four cardinal directions (up, down, left, right)
+    pub fn neighbours(&self) -> [Position; 4] {
+        [
+            self.position_in_direction(Up),
+            self.position_in_direction(Down),
+            self.position_in_direction(Left),
+            self.position_in_direction(Right),
+        ]
+    }
+
+    /// Return the position next to the current one in the given direction
+    ///
+    /// Interpretation of directions:
+    /// Right -> x gets higher
+    /// Left -> x gets lower
+    /// Up -> y gets higher
+    /// Down -> y gets lower
+    pub fn position_in_direction(&self, dir: Direction) -> Position {
+        match dir {
+            Left => *self + (-1, 0),
+            Right => *self + (1, 0),
+            Up => *self + (0, 1),
+            Down => *self + (0, -1)
+        }
+    }
+
+    /// Creates an iterator from all positions between self and the given end.
+    ///
+    /// The two positions form a square. For example (0,0) and (4,3) form the following shape
+    ///
+    /// XXXX
+    /// XXXX
+    /// XXXX
+    ///
+    /// The iterator takes every position from left to right and bottom to top. So the first element will be (0,0),
+    /// followed by (1,0) until (3,0). Then (0,1), (1,1) and so on until (3,3) was returned
+    pub fn iter_to(&self, other: Position) -> PositionIter {
+        PositionIter::new(*self, other)
+    }
+}
+
+impl Add for Position {
+    type Output = Position;
+
+    fn add(self, other: Self) -> Self::Output {
+        Position::new(
+            self.x + other.x,
+            self.y + other.y,
+        )
+    }
+}
+
+impl Add<(isize, isize)> for Position {
+    type Output = Position;
+
+    fn add(self, (x, y): (isize, isize)) -> Self::Output {
+        Position::new(
+            self.x + x,
+            self.y + y,
+        )
+    }
+}
+
+impl From<(isize, isize)> for Position {
+    fn from((x, y): (isize, isize)) -> Self {
+        Position::new(x, y)
+    }
+}
+
+impl From<(usize, usize)> for Position {
+    fn from((x, y): (usize, usize)) -> Self {
+        Position::new(x as isize, y as isize)
+    }
+}
+
+pub struct PositionIter {
+    current_x: isize,
+    current_y: isize,
+    start: Position,
+    end: Position,
+}
+
+impl PositionIter {
+    fn new(start: Position, end: Position) -> Self {
+        if start > end {
+            panic!("start must be less or equal to end")
+        }
+
+        PositionIter
+        {
+            current_x: start.x,
+            current_y: start.y,
+            start,
+            end,
+        }
+    }
+
+    fn is_finished(&self) -> bool {
+        self.current_x > self.end.x || self.current_y > self.end.y
+    }
+
+    fn increment(&mut self) {
+        self.current_x += 1;
+
+        if self.current_x > self.end.x && self.current_y < self.end.y {
+            self.current_x = self.start.x;
+            self.current_y += 1;
+        }
+    }
+}
+
+impl Iterator for PositionIter {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_finished() {
+            return None;
+        }
+
+        let current = Position::new(self.current_x, self.current_y);
+        self.increment();
+        Some(current)
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Direction {
     Left,
     Right,
     Up,
     Down,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Position;
+    use crate::Direction::*;
+
+    #[test]
+    fn add_position_works() {
+        let pos = Position::new(1, 2);
+        let other = Position::new(2, 3);
+        assert_eq!(Position::new(3, 5), pos + other);
+    }
+
+    #[test]
+    fn add_tuple_works() {
+        let pos = Position::new(1, 2);
+        let other = (2, 3);
+        assert_eq!(Position::new(3, 5), pos + other);
+    }
+
+    #[test]
+    fn get_position_in_direction_works() {
+        let pos = Position::new(0, 0);
+
+        [
+            (Up, Position::new(0, 1)),
+            (Down, Position::new(0, -1)),
+            (Left, Position::new(-1, 0)),
+            (Right, Position::new(1, 0)),
+        ]
+            .into_iter()
+            .for_each(|(dir, expected)| assert_eq!(pos.position_in_direction(dir), expected))
+    }
+
+    #[test]
+    fn neighbours_works() {
+        let pos = Position::new(0, 0);
+
+        let neighbours = pos.neighbours();
+
+        [
+            Position::new(0, 1),
+            Position::new(0, -1),
+            Position::new(-1, 0),
+            Position::new(1, 0),
+        ].into_iter().for_each(|expected| assert!(neighbours.contains(&expected)))
+    }
+
+    #[test]
+    fn neighbours_in_bounds_work() {
+        let pos = Position::new(1, 1);
+        let neighbours = pos.neighbours_in_bounds(Position::new(0, 0), Position::new(2, 2)).into_iter().collect::<Vec<_>>();
+
+        assert_eq!(neighbours.len(), 4);
+        assert!(neighbours.contains(&Position::new(2, 1)));
+        assert!(neighbours.contains(&Position::new(0, 1)));
+        assert!(neighbours.contains(&Position::new(1, 2)));
+        assert!(neighbours.contains(&Position::new(1, 0)));
+
+        let pos = Position::new(0, 0);
+        let neighbours = pos.neighbours_in_bounds(Position::new(0, 0), Position::new(1, 1)).into_iter().collect::<Vec<_>>();
+
+        assert_eq!(neighbours.len(), 2);
+        assert!(neighbours.contains(&Position::new(1, 0)));
+        assert!(neighbours.contains(&Position::new(0, 1)));
+    }
+
+    #[test]
+    fn position_iter_works() {
+        [
+            (
+                Position::new(0, 0),
+                Position::new(0, 0),
+                vec![Position::new(0, 0)]
+            ),
+            (
+                Position::new(-1, -1),
+                Position::new(1, 1),
+                [(-1isize, -1isize), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)].into_iter().map(From::from).collect()
+            ),
+            (
+                Position::new(0, 0),
+                Position::new(2, 0),
+                [(0isize, 0isize), (1, 0), (2, 0)].into_iter().map(From::from).collect()
+            ),
+            (
+                Position::new(0, 0),
+                Position::new(0, 2),
+                [(0isize, 0isize), (0, 1), (0, 2)].into_iter().map(From::from).collect()
+            )
+        ]
+            .into_iter()
+            .for_each(|(start, end, expected)| assert_eq!(start.iter_to(end).collect::<Vec<_>>(), expected))
+    }
 }
